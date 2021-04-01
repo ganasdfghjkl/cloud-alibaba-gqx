abd07ff1a8a81229ccf9c0585cb8eb7c96e1805f
jegqx <ganqixin>
2021-04-01 16:43:03 +0800

    ......

---
+++ cloud-gqx/cloud-gqx-starters/gqx-cloud-druid-sources-starter/src/main/java/com/gqx/cloud/druid/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.gqx.cloud.druid.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnMissingBean(name = "dynamicDataSource")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+
+}
