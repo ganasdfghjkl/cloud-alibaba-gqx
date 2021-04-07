9ac779d0ad6ae5f2f42de3d7901d3ba926e69c99
jegqx <ganqixin>
2021-04-07 17:24:05 +0800

    ......

--- cloud-gqx/cloud-gqx-boot/cloud-gqx-servers/test-server-three/src/main/java/com/gqx/cloud/serverthree/ServerThreeApplication.java
+++ cloud-gqx/cloud-gqx-boot/cloud-gqx-servers/test-server-three/src/main/java/com/gqx/cloud/serverthree/ServerThreeApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
+ * TODO https://blog.csdn.net/tengxing007/article/details/106550918 动态配置中心
+ * @author
  */
 @SpringBootApplication
 public class ServerThreeApplication {
